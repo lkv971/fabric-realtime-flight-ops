1d02336885a83033f744a7728c0492bdd7953099
Lee-roy <leeroyvaillant@outlook.com>
2025-08-03 23:55:07 -0400

    Add column to table

--- realtime-flight-ops/chore/WH_Flights.Warehouse/Aviation/Tables/IngestionLogs.sql
+++ realtime-flight-ops/chore/WH_Flights.Warehouse/Aviation/Tables/IngestionLogs.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [Aviation].[IngestionLogs] (
 
-	[PipelineName] varchar(100) NULL, 
+	[TableName] varchar(100) NULL, 
 	[ProcessedTime] datetime2(3) NULL, 
+	[RecordCount] int NULL, 
 	[Status] varchar(100) NULL
 );
